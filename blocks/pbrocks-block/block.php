7fa05872c6c5dab37e18400d0c31a65017e05a68
Paul Barthmaier <paul@barthmaier.rocks>
2019-01-06 22:47:07 -0500

    adjusting comments

--- blocks/pbrocks-block/block.php
+++ blocks/pbrocks-block/block.php
@@ -10,9 +10,9 @@ defined( 'ABSPATH' ) || die( 'File cannot be accessed directly' );
 /**
  * Normally blocks are registered in JS, but here are mirroring the registration of our block in php, since we want to render via php. In this way, it is here, rather than in JS that e explicitly define the attributes for the block.
  *
- * We create a shortcode, [pbrocks-exploring-shortcode], which accepts three arguments, 'value_one', 'value_two', and  'value_three'. This shortcode would be used like so:
+ * We create a shortcode, [pbrocks-exploring-shortcode], which accepts one argument, 'value_one'. This shortcode would be used like so:
  *
- * [pbrocks-exploring-shortcode value_one="Input some value_one attribute value" value_two="Input some value_two attribute value" value_three="Input some value_three attribute value"]
+ * [pbrocks-exploring-shortcode value_one="Input some value_one attribute value"]
  */
 
 /**
