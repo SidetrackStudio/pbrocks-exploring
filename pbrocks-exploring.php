03cec5a4ddc35496cf202091ead8cc1a2962bb9b
Paul Barthmaier <paul@barthmaier.rocks>
2019-01-06 22:28:45 -0500

    ignoring js

--- pbrocks-exploring.php
+++ pbrocks-exploring.php
@@ -5,7 +5,7 @@
  * Author: pbrocks
  * Author URI: https://pbrocks.sidetrack.studio/exploring-the-block-api/
  * Description: Exploring the Blocks API creates a basic WP Block and uses PHP to deliver a shortcode with properties.
- * Version: 1.9
+ * Version: 1.0.1
  * License: GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: pbrocks-exploring
